f4673b74318d4d3e503db0063f9766db82e77022
Khrys Samsonova <65388525+Khrys-Samsonova@users.noreply.github.com>
2021-08-29 15:53:32 +1200

    php test
    very basic website to test with database

---
+++ www/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head><title>Database test page</title>
+<style>
+th { text-align: left; }
+
+table, th, td {
+  border: 2px solid grey;
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 0.2em;
+}
+</style>
+</head>
+
+<body>
+<h1>Database test page</h1>
+
+<p>Showing contents of users table:</p>
+
+<table border="1">
+<tr><th>User Name</th><th>User Address</th></tr>
+
+<?php
+ 
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+$q = $pdo->query("SELECT * FROM users");
+
+while($row = $q->fetch()){
+  echo "<tr><td>".$row["user_name"]."</td><td>".$row["user_address"]."</td></tr>\n";
+}
+
+?>
+</table>
+</body>
+</html>
