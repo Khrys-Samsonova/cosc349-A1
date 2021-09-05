a245c834a4c4453cd655e92725efab3ce8bed644
Khrys Samsonova <65388525+Khrys-Samsonova@users.noreply.github.com>
2021-09-05 14:48:49 +1200

    added test data to sql and index page

--- setup-database.sql
+++ setup-database.sql
@@ -4,10 +4,6 @@ create table users (
     user_password varchar(64),
     user_address varchar(256),
     PRIMARY KEY (user_ID)
-    /* 
-    add photo field
-    add dietary requirements?
-    */
 );
 
 create table restaurants (
@@ -28,13 +24,8 @@ create table meals (
     delivery_options boolean default 0,
     restaurant_meal_ID varchar(10),
     PRIMARY KEY (meal_ID)
-    
     /*
-    
     foreign key (restaurant_meal_ID) references restaurants(restaurant_ID),
-    add photo field
-    add allergens? - extension
-    add meal photo
     */
 );
 
@@ -45,7 +36,6 @@ create table orders (
     PRIMARY KEY (order_ID)
     
 );
-
   /* 
     constraint fk_meal_orders foreign key (meal_ID) references meals(meal_ID),
     constraint fk_users foreign key (user_ID) references users(user_ID),
@@ -53,5 +43,22 @@ create table orders (
     user_ID varchar
   */
 
-INSERT INTO users (user_ID, user_name, user_password, user_address) VALUES ('01', 'Khrys','123', '309b George st');
-INSERT INTO meals (meal_ID, meal_name, meal_description, meal_made_date, meal_perishable, meal_perish_date) VALUES ('01', 'Stew', 'Vege Stew','2021-08-28','1','2021-08-31' );
+/*
+  Test Data for all tables
+ */ 
+INSERT INTO users (user_ID, user_name, user_password, user_address) VALUES ('001', 'Khrys','123', '309b George st');
+INSERT INTO users (user_ID, user_name, user_password, user_address) VALUES ('002', 'Cathy','321', '660 Castle st');
+INSERT INTO users (user_ID, user_name, user_password, user_address) VALUES ('003', 'Emily','abc123', '110a Leith st');
+
+INSERT INTO restaurants (restaurant_ID, restaurant_name, restaurant_password, restaurant_address) VALUES ('abc1', 'Kind Company','12345', '333 George st');
+INSERT INTO restaurants (restaurant_ID, restaurant_name, restaurant_password, restaurant_address) VALUES ('001', 'Hikari Sushi Bar','hikaribar', '363 George st');
+INSERT INTO restaurants (restaurant_ID, restaurant_name, restaurant_password, restaurant_address) VALUES ('a1b2c3', 'Zanzibar','abc123', '311 George st');
+
+/* please note that delivery options: 0 is false (pickup) and 1 is true (delivery) */
+INSERT INTO meals (meal_ID, meal_name, meal_description, meal_made_date, meal_perishable, meal_perish_date, delivery_options) VALUES ('01', 'Stew', 'Vege Stew','2021-08-28','1','2021-08-31', '0');
+INSERT INTO meals (meal_ID, meal_name, meal_description, meal_made_date, meal_perishable, meal_perish_date, delivery_options) VALUES ('02', 'Green Curry', 'Thai Seafood Curry','2021-08-31','1','2021-09-02', '1');
+INSERT INTO meals (meal_ID, meal_name, meal_description, meal_made_date, meal_perishable, meal_perish_date, delivery_options) VALUES ('03', 'Cake', 'Chocolate cake','2021-09-3','1','2021-09-10', '0');
+INSERT INTO meals (meal_ID, meal_name, meal_description, meal_made_date, meal_perishable, meal_perish_date, delivery_options) VALUES ('04', 'Sushi', 'Salmon, Avocado Sushi','2021-09-01','1','2021-09-01', '1');
+
+INSERT INTO orders (order_ID, order_date, order_delivered) VALUES ('001','2021-09-03 12:32:03', '0');
+INSERT INTO orders (order_ID, order_date, order_delivered) VALUES ('002','2021-09-05 09:50:48', '1');
--- www/index.php
+++ www/index.php
@@ -34,6 +34,7 @@ $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
 $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
+# user query
 $q = $pdo->query("SELECT * FROM users");
 
 while($row = $q->fetch()){
@@ -42,5 +43,79 @@ while($row = $q->fetch()){
 
 ?>
 </table>
+
+<table border="1">
+<tr><th>Restaurant</th><th>Restaurant Address</th></tr>
+
+<?php
+ 
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+# restaurants query
+$q_r = $pdo->query("SELECT * FROM restaurants");
+
+while($row = $q_r->fetch()){
+  echo "<tr><td>".$row["restaurant_name"]."</td><td>".$row["restaurant_address"]."</td></tr>\n";
+}
+
+?>
+</table>
+
+<table border="1">
+<tr><th>Meal Name</th><th>Meal Description</th><th>Date Made</th><th>Use By</th><th>Delivery</th></tr>
+
+<?php
+ 
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+# meals query
+$q_m = $pdo->query("SELECT * FROM meals");
+
+while($row = $q_m->fetch()){
+  echo "<tr><td>".$row["meal_name"]."</td><td>".$row["meal_description"]."</td><td>".$row["meal_made_date"]."</td><td>".$row["meal_perish_date"]."</td><td>".$row["delivery_options"]."</td></tr>\n";
+}
+
+?>
+</table>
+
+<table border="1">
+<tr><th>Order ID</th><th>Time Placed</th></tr>
+
+<?php
+ 
+$db_host   = '192.168.2.12';
+$db_name   = 'fvision';
+$db_user   = 'webuser';
+$db_passwd = 'insecure_db_pw';
+
+$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+
+$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+
+# orders query
+$q_o = $pdo->query("SELECT * FROM orders");
+
+while($row = $q_o->fetch()){
+  echo "<tr><td>".$row["order_ID"]."</td><td>".$row["order_date"]."</td></tr>\n";
+}
+
+?>
+</table>
+
+
 </body>
 </html>
