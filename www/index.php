6c8c350432f905338ba66045efb7f243263f1504
Khrys Samsonova <65388525+Khrys-Samsonova@users.noreply.github.com>
2021-09-06 11:29:27 +1200

    added test stylesheet

--- www/index.php
+++ www/index.php
@@ -1,18 +1,8 @@
 <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
-<head><title>Database test page</title>
-<style>
-th { text-align: left; }
-
-table, th, td {
-  border: 2px solid grey;
-  border-collapse: collapse;
-}
-
-th, td {
-  padding: 0.2em;
-}
-</style>
+<head>
+<title>Database test page</title>
+<link rel="stylesheet" href="style.css">
 </head>
 
 <body>
